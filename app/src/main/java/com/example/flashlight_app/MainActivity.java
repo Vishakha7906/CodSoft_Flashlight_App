e48348f79d5be18115872a4b188d86ee6468f496
VISHAKHA SAINI <[vishakhasaini7906@gmail.com]>
2023-08-04 14:05:51 +0530

    final commit

---
+++ app/src/main/java/com/example/flashlight_app/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.flashlight_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Switch aSwitch;
+    TextView textView;
+    CameraManager cameraManager;
+    String cameraid , result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        aSwitch = findViewById(R.id.on_off_switch);
+        textView = findViewById(R.id.tv_result);
+
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                torch(isChecked);
+
+
+            }
+        });
+
+
+    }
+
+    private void torch(boolean isChecked) {
+
+        try {
+            cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
+            cameraid = cameraManager.getCameraIdList()[0];
+            cameraManager.setTorchMode(cameraid,isChecked);
+            result = isChecked? "ON" : "OFF";
+            textView.setText(result);
+        } catch (CameraAccessException e) {
+           e.printStackTrace();
+        }
+    }
+
+}
